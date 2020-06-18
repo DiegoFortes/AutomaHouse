edc07f2abe700cc7f383b2d487d4de1cef19b8d1
DiegoFortes <48811873+DiegoFortes@users.noreply.github.com>
2020-06-17 23:08:30 -0300

    Read updates from database

--- app/src/main/java/com/example/automahouse/Lampada.java
+++ app/src/main/java/com/example/automahouse/Lampada.java
@@ -1,20 +1,21 @@
 package com.example.automahouse;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.Switch;
-import android.widget.Toast;
 
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.firebase.auth.FirebaseAuth;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 
 public class Lampada extends AppCompatActivity implements View.OnClickListener {
@@ -34,18 +35,59 @@ public class Lampada extends AppCompatActivity implements View.OnClickListener {
         Switch switch4 = findViewById(R.id.lamp4);
         Switch switch5 = findViewById(R.id.lamp5);
 
+        // mapeia nome da lâmpada para id do switch
+        HashMap<String, Integer> switchIdsByText = new HashMap<>();
+        switchIdsByText.put(switch1.getText().toString(), R.id.lamp1);
+        switchIdsByText.put(switch2.getText().toString(), R.id.lamp2);
+        switchIdsByText.put(switch3.getText().toString(), R.id.lamp3);
+        switchIdsByText.put(switch4.getText().toString(), R.id.lamp4);
+        switchIdsByText.put(switch5.getText().toString(), R.id.lamp5);
+
+        // mapeia id do switch para id da imagem
+        HashMap<Integer, Integer> imageIdsBySwitchId = new HashMap<>();
+        imageIdsBySwitchId.put(R.id.lamp1, R.id.ic_lamp1);
+        imageIdsBySwitchId.put(R.id.lamp2, R.id.ic_lamp2);
+        imageIdsBySwitchId.put(R.id.lamp3, R.id.ic_lamp3);
+        imageIdsBySwitchId.put(R.id.lamp4, R.id.ic_lamp4);
+        imageIdsBySwitchId.put(R.id.lamp5, R.id.ic_lamp5);
+
         switch1.setOnClickListener(this);
         switch2.setOnClickListener(this);
         switch3.setOnClickListener(this);
         switch4.setOnClickListener(this);
         switch5.setOnClickListener(this);
 
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Iterable<DataSnapshot> lamps = dataSnapshot.getChildren();
+                for (DataSnapshot lamp : lamps) {
+                    Integer isOn = lamp.getValue(Integer.class);
+                    String lampName = lamp.getKey();
+                    Integer lampId = switchIdsByText.get(lampName);
+                    Switch lampSwitch = findViewById(lampId);
+                    if (lampSwitch.isChecked() ^ isOn.equals(1)) {
+                        ImageView icon = findViewById(imageIdsBySwitchId.get(lampId));
+//                        icon.setImageDrawable(getResources().getDrawable(R.drawable.ic_lamp_on));
+                        lampSwitch.setChecked(isOn.equals(1));
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("Error getting data from DB.");
+            }
+        };
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
+        myRef.addValueEventListener(postListener);
+
         Button btnLogout = (Button) findViewById(R.id.logout1);
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Lampada.this, MainActivity.class);
-                startActivity( intent );
+                startActivity(intent);
             }
         });
     }
@@ -67,6 +109,7 @@ public class Lampada extends AppCompatActivity implements View.OnClickListener {
         String lampName = lampSwitch.getText().toString();
         myRef = database.getReference(lampName);
         ImageView imgSwitch = null;
+
         switch (v.getId()) {
             case R.id.lamp1:
                 imgSwitch = findViewById(R.id.ic_lamp1);
